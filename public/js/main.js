c4c5508934711462baceffa992a51b9a7bac598e
Barış Demirezen <Baris.Demirezen@hesapkurdu.com>
2021-06-15 23:06:08 +0300

    added api preview area will fix beautify

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
 const navbar = document.querySelector('.navbar');
+const requestAddress = document.querySelector('#request-address');
+const requestButton = document.querySelector('#request-button');
+const responseArea = document.querySelector('#response-area');
+
 let status;
 window.onscroll = function(e) {
     status = this.oldScroll < this.scrollY;
@@ -12,3 +16,11 @@ window.onscroll = function(e) {
     }
     this.oldScroll = this.scrollY
   }
+
+requestButton.onclick = function(e) {
+    let response = '';
+    fetch(`${document.location.origin}/api/${requestAddress.value}`)
+    .then( (res) => res.json())
+    .then( (result) => response = JSON.stringify(result))
+    .then(() => responseArea.innerHTML = response)  
+}
